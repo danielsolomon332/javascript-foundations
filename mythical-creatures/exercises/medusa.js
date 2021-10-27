439a693eddb24140284225321ac89a1f0e25c479
danielsolomon332 <daniel.solomon332@gmail.com>
2021-10-27 16:01:43 -0600

    Finish medusa test

--- mythical-creatures/exercises/medusa.js
+++ mythical-creatures/exercises/medusa.js
@@ -1,2 +1,26 @@
 var Person = require('./person');
 var Statue = require('./statue');
+
+class Medusa {
+  constructor(name) {
+    this.name = name;
+    this.statues = [];
+  }
+
+  gazeAtVictim(victim) {
+    if (this.statues.length <= 2) {
+      var newVictim = new Statue(victim.name);
+      this.statues.push(newVictim);
+    } else if (this.statues.length = 3) {
+      var newVictim = new Statue(victim.name);
+      this.statues.push(newVictim);
+      var freeVictim = Object.assign(victim, this.statues[0]);
+      freeVictim.mood = 'relieved'
+      this.statues.shift();
+      return freeVictim
+    }
+  }
+}
+
+
+module.exports = Medusa;
