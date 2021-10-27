787da2da2105c0d866e708be163abb7612e924e9
danielsolomon332 <daniel.solomon332@gmail.com>
2021-10-26 21:52:27 -0600

    Finish dragon test

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -2,9 +2,22 @@ class Dragon {
   constructor(name, rider) {
     this.name = name;
     this.rider = rider;
-console.log(name, rider)
+    this.hungry = true;
+    this.hungryMeter = 0;
   }
-}
+
+  greet() {
+    return `Hi, ${this.rider}!`
+  }
+  
+  eat() {
+    this.hungryMeter += 1
+      if (this.hungryMeter >= 3) {
+        return this.hungry = false;
+      }
+    }
+  }
+
 
 
 module.exports = Dragon;
