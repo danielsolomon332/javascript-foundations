36f6f69587eae5125c6b0db36283a218caf4d0e7
danielsolomon332 <daniel.solomon332@gmail.com>
2021-11-08 19:08:40 -0700

    Finish Lunchbox

--- lunchbox/lunchbox.js
+++ lunchbox/lunchbox.js
@@ -1,5 +1,24 @@
 class LunchBox {
-
+  constructor(nameBox) {
+    this.owner = nameBox.owner;
+    this.material = nameBox.madeOf;
+    this.shape = nameBox.shape;
+    this.color = nameBox.color;
+    this.snacks = [];
+  }
+  acquireSnack(snack) {
+    this.snacks.push(snack);
+    snack.isInLunchBox = true;
+  }
+  findHealthySnacks() {
+    var healthyArray = [];
+    for (var i = 0; i < this.snacks.length; i++) {
+      if (this.snacks[i].checkForHealthy()) {
+        healthyArray.push(this.snacks[i].type)
+      }
+    }
+    return healthyArray
+  }
 }
 
 module.exports = LunchBox;
--- lunchbox/snack.js
+++ lunchbox/snack.js
@@ -1,5 +1,28 @@
 class Snack {
-
+  constructor(type) {
+    this.deliciousLevel = "extra";
+    this.type = type;
+    this.amount = 100;
+    this.isInLunchBox = false;
+    this.cuttingItClose = false;
+  }
+  getEaten() {
+    if (this.amount > 20) {
+    this.amount -= 10;
+    }
+    if (this.amount === 20) {
+    this.cuttingItClose = true;
+    }
+  }
+  checkForHealthy() {
+    return this.type.toUpperCase().includes("FRUIT")
+    // if (this.type.toUpperCase().includes("FRUIT")) {
+    // if (this.type.includes("Fruit") || this.type.includes("fruit")) {
+    //   return true
+    // } else {
+    //   return false
+    // }
+  }
 }
 
 module.exports = Snack;
