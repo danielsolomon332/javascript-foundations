a90c28a5c48be2d9fa80e58b3a311c46e198f6f7
danielsolomon332 <daniel.solomon332@gmail.com>
2021-11-10 09:42:03 -0700

    Finish roadrace

--- roadrace/src/runner.js
+++ roadrace/src/runner.js
@@ -1,5 +1,26 @@
 class Runner {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+    this.milesRun = 0;
+    this.fitness = 25;
+    this.completedRaces = [];
+  }
 
+  runSomeMiles(milesRun) {
+    this.milesRun += milesRun;
+    this.fitness += milesRun;
+  }
+
+  stretch() {
+    this.fitness += 0.5;
+  }
+
+  runRace(raceName, raceMiles) {
+    this.completedRaces.push(raceName);
+    this.runSomeMiles(raceMiles);
+
+  }
 }
 
 module.exports = Runner;
